d2591e0185f7f306ebe730439891dd1421a841f7
장준호 <junho335276@gmail.com>
2023-05-17 20:47:40 +0900

    login update

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index');
+    res.render('index',{message: null });
 });
 router.get('/join',function(req,res){
   res.render('join',{message: null })
@@ -14,24 +14,24 @@ router.post('/api/join', async function(req,res,rows){
   var[rows] = await connection.query("select * from user where id=?",[req.body.id])
   if(rows.length==0){ //중복된 아이디 없음: 회원가입 가능
     var[rows] = await connection.query("insert into user (id,name,email,password) values (?,?,?,?)", 
-    [req.body.id, req.body.name, req.body.email, req.body.password])
- 
+      [req.body.id, req.body.name, req.body.email, req.body.password])
+    
+    res.redirect('/')
   }else{//아이디 중복: 회원가입 불가
     res.render('join',{message: "이미 가입되어있는 아이디 입니다."})
   }
-  
-  /* connection.query("select * from user where id=?",[req.body.id],function(err,rows){
-    console.log(err,rows)
-    if(rows.length==0){ //중복된 아이디 없음: 회원가입 가능
-      connection.query("insert into user (id,name,email,password) values (?,?,?,?)", 
-      [req.body.id, req.body.name, req.body.email, req.body.password],function(err,rows){
-        console.log(err,rows)
-      })
-    }else{ //아이디 중복: 회원가입 불가
+})
 
-    } */
+router.post('api/login',async function(req, res){
+  console.log(req.body)
+  var [rows] = await connection.query("select * from user where id=? and password=?",
+    [req.body.id, req.body.password])
+  if(rows.length==0){//로그인 실패 - 아이디 또는 패스워드가 틀렸을 경우
+    res.render('index', {message: "아이디 또는 패스워드가 틀렸습니다."})
 
-  //})
+  }else{//로그인 성공
+    res.redirect('/main')
+  }
 })
 
 
